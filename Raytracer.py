0c005087413195f82156781c449acae385a8a425
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2022-10-25 00:45:00 -0600

    Render HD done

--- Raytracer.py
+++ Raytracer.py
@@ -4,8 +4,8 @@ from figures import *
 from lights import *
 
 
-width = 512
-height = 320
+width = 1080
+height = 720
 
 centrox = 0
 centroy = -1
@@ -13,30 +13,32 @@ centroz = 0
 
 # Materiales
 
-aluminium = Material(diffuse=(0.3, 0.3, 0.3), spec=64, matType=REFLECTIVE)
+aluminium = Material(diffuse=(0.9, 0.9, 0.9))
+nasa = Material(texture=Texture("txnasa.bmp"))
 stone = Material(diffuse=(0.4, 0.4, 0.4), spec=8)
 earth = Material(texture=Texture("txearthDay.bmp"))
 moon = Material(texture=Texture("txmoon.bmp"))
 sun = Material(texture=Texture("txsun.bmp"))
 saturn = Material(texture=Texture("txsaturn.bmp"))
 jupiter = Material(texture=Texture("txjupiter.bmp"))
-mars = Material(texture=Texture("txmars.bmp"), diffuse=(0.8, 0, 0),
-                spec=16, matType=REFLECTIVE)
+mars = Material(texture=Texture("txmars.bmp"))
 venus = Material(texture=Texture("txvenus.bmp"))
-uranus = Material(texture=Texture("txuranus.bmp"))
+uranus = Material(texture=Texture("txuranus.bmp"), diffuse=(0.3, 0.8, 0.8),
+                  spec=16, matType=REFLECTIVE)
 pluton = Material(texture=Texture("txpluton.bmp"))
 mercury = Material(texture=Texture("txmercury.bmp"))
-mirror = Material(diffuse=(0.9, 0.9, 0.9), spec=64, matType=REFLECTIVE)
-glass = Material(diffuse=(0.9, 0.9, 0.9), spec=64,
+glass = Material(diffuse=(0.7, 0.7, 0), spec=64,
                  ior=1.5, matType=TRANSPARENT)
 
 rtx = Raytracer(width, height)
 rtx.envMap = Texture("Galaxia2.bmp")
 
-rtx.lights.append(AmbientLight(intensity=0.1))
+# Lights
+rtx.lights.append(AmbientLight(intensity=0.2))
 rtx.lights.append(PointLight(point=(-5, 5, -35), attenuation=1.0))
 rtx.lights.append(PointLight(point=(-5, 5, -65), attenuation=1.0))
-# rtx.lights.append(PointLight(point=(-10, -2, 20), attenuation=0.1))
+
+#               PLANETS
 
 # Jupiter
 rtx.scene.append(Sphere(V3(10, -4, -10), 6, jupiter))
@@ -70,16 +72,16 @@ rtx.scene.append(Sphere(V3(-8, 4.5, -16), 1, pluton))
 rtx.scene.append(Sphere(V3(-8, 11, -25), 1, mercury))
 
 # Satelite
-rtx.scene.append(Sphere(V3(-3, -1, -5), 0.5, glass))
+rtx.scene.append(Sphere(V3(-3, -0.8, -5), 0.5, glass))
 rtx.scene.append(AABB(position=(-3, -1.5, -5),
-                 size=(1, 1, 1), material=aluminium))
-rtx.scene.append(AABB(position=(-3.4, -2.5, -4.6),
+                 size=(1, 1, 1), material=nasa))
+rtx.scene.append(AABB(position=(-3.4, -2, -4.6),
                  size=(0.1, 1, 0.2), material=aluminium))
-rtx.scene.append(AABB(position=(-3.4, -2.5, -5.4),
+rtx.scene.append(AABB(position=(-3.4, -2, -5.4),
                  size=(0.1, 1, 0.2), material=aluminium))
-rtx.scene.append(AABB(position=(-2.6, -2.5, -4.6),
+rtx.scene.append(AABB(position=(-2.6, -2, -4.6),
                  size=(0.1, 1, 0.2), material=aluminium))
-rtx.scene.append(AABB(position=(-2.6, -2.5, -5.4),
+rtx.scene.append(AABB(position=(-2.6, -2, -5.4),
                  size=(0.1, 1, 0.2), material=aluminium))
 
 rtx.glRender()
