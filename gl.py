2e9bf1b42381abcb68fe88e5ac9c4b1e8eb7c938
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2022-10-28 00:15:57 -0600

    More glMath changes

--- gl.py
+++ gl.py
@@ -1,6 +1,7 @@
 import struct
 from collections import namedtuple
 import numpy as np
+import glMath as gm
 from figures import *
 from lights import *
 from math import cos, sin, tan, pi
@@ -14,22 +15,27 @@ V2 = namedtuple('Point2', ['x', 'y'])
 V3 = namedtuple('Point3', ['x', 'y', 'z'])
 V4 = namedtuple('Point4', ['x', 'y', 'z', 'w'])
 
+
 def char(c):
-    #1 byte
+    # 1 byte
     return struct.pack('=c', c.encode('ascii'))
 
+
 def word(w):
-    #2 bytes
+    # 2 bytes
     return struct.pack('=h', w)
 
+
 def dword(d):
-    #4 bytes
+    # 4 bytes
     return struct.pack('=l', d)
+
 
 def color(r, g, b):
     return bytes([int(b * 255),
                   int(g * 255),
-                  int(r * 255)] )
+                  int(r * 255)])
+
 
 def baryCoords(A, B, C, P):
 
@@ -49,6 +55,7 @@ def baryCoords(A, B, C, P):
     else:
         return u, v, w
 
+
 class Raytracer(object):
     def __init__(self, width, height):
 
@@ -57,19 +64,18 @@ class Raytracer(object):
 
         self.fov = 60
         self.nearPlane = 0.1
-        self.camPosition = V3(0,0,0)
+        self.camPosition = V3(0, 0, 0)
 
-        self.scene = [ ]
-        self.lights = [ ]
+        self.scene = []
+        self.lights = []
 
         self.envMap = None
 
+        self.clearColor = color(0, 0, 0)
+        self.currColor = color(1, 1, 1)
 
-        self.clearColor = color(0,0,0)
-        self.currColor = color(1,1,1)
+        self.glViewport(0, 0, self.width, self.height)
 
-        self.glViewport(0,0,self.width, self.height)
-        
         self.glClear()
 
     def glViewport(self, posX, posY, width, height):
@@ -79,22 +85,21 @@ class Raytracer(object):
         self.vpHeight = height
 
     def glClearColor(self, r, g, b):
-        self.clearColor = color(r,g,b)
+        self.clearColor = color(r, g, b)
 
     def glColor(self, r, g, b):
-        self.currColor = color(r,g,b)
+        self.currColor = color(r, g, b)
 
     def glClear(self):
-        self.pixels = [[ self.clearColor for y in range(self.height)]
-                         for x in range(self.width)]
+        self.pixels = [[self.clearColor for y in range(self.height)]
+                       for x in range(self.width)]
 
-    def glClearViewport(self, clr = None):
+    def glClearViewport(self, clr=None):
         for x in range(self.vpX, self.vpX + self.vpWidth):
             for y in range(self.vpY, self.vpY + self.vpHeight):
-                self.glPoint(x,y,clr)
+                self.glPoint(x, y, clr)
 
-
-    def glPoint(self, x, y, clr = None): # Window Coordinates
+    def glPoint(self, x, y, clr=None):  # Window Coordinates
         if (0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[x][y] = clr or self.currColor
 
@@ -112,7 +117,7 @@ class Raytracer(object):
 
         return intersect
 
-    def cast_ray(self, orig, dir, sceneObj = None, recursion = 0):
+    def cast_ray(self, orig, dir, sceneObj=None, recursion=0):
         intersect = self.scene_intersect(orig, dir, sceneObj)
 
         if intersect == None or recursion >= MAX_RECURSION_DEPTH:
@@ -125,7 +130,7 @@ class Raytracer(object):
 
         material = intersect.sceneObj.material
 
-        finalColor = np.array([0,0,0])
+        finalColor = np.array([0, 0, 0])
         objectColor = np.array([material.diffuse[0],
                                 material.diffuse[1],
                                 material.diffuse[2]])
@@ -142,12 +147,14 @@ class Raytracer(object):
 
         elif material.matType == REFLECTIVE:
             reflect = reflectVector(intersect.normal, np.array(dir) * -1)
-            reflectColor = self.cast_ray(intersect.point, reflect, intersect.sceneObj, recursion + 1)
+            reflectColor = self.cast_ray(
+                intersect.point, reflect, intersect.sceneObj, recursion + 1)
             reflectColor = np.array(reflectColor)
 
-            specColor = np.array([0,0,0])
+            specColor = np.array([0, 0, 0])
             for light in self.lights:
-                specColor = np.add(specColor, light.getSpecColor(intersect, self))
+                specColor = np.add(
+                    specColor, light.getSpecColor(intersect, self))
 
             finalColor = reflectColor + specColor
 
@@ -155,31 +162,37 @@ class Raytracer(object):
             outside = np.dot(dir, intersect.normal) < 0
             bias = intersect.normal * 0.001
 
-            specColor = np.array([0,0,0])
+            specColor = np.array([0, 0, 0])
             for light in self.lights:
-                specColor = np.add(specColor, light.getSpecColor(intersect, self))
+                specColor = np.add(
+                    specColor, light.getSpecColor(intersect, self))
 
             reflect = reflectVector(intersect.normal, np.array(dir) * -1)
-            reflectOrig = np.add(intersect.point, bias) if outside else np.subtract(intersect.point, bias)
-            reflectColor = self.cast_ray(reflectOrig, reflect, None, recursion + 1)
+            reflectOrig = np.add(intersect.point, bias) if outside else np.subtract(
+                intersect.point, bias)
+            reflectColor = self.cast_ray(
+                reflectOrig, reflect, None, recursion + 1)
             reflectColor = np.array(reflectColor)
 
             kr = fresnel(intersect.normal, dir, material.ior)
 
-            refractColor = np.array([0,0,0])
+            refractColor = np.array([0, 0, 0])
             if kr < 1:
                 refract = refractVector(intersect.normal, dir, material.ior)
-                refractOrig = np.subtract(intersect.point, bias) if outside else np.add(intersect.point, bias)
-                refractColor = self.cast_ray(refractOrig, refract, None, recursion + 1)
+                refractOrig = np.subtract(
+                    intersect.point, bias) if outside else np.add(intersect.point, bias)
+                refractColor = self.cast_ray(
+                    refractOrig, refract, None, recursion + 1)
                 refractColor = np.array(refractColor)
 
-            finalColor = reflectColor * kr + refractColor * (1 - kr) + specColor
-
+            finalColor = reflectColor * kr + \
+                refractColor * (1 - kr) + specColor
 
         finalColor *= objectColor
 
         if material.texture and intersect.texcoords:
-            texColor = material.texture.getColor(intersect.texcoords[0], intersect.texcoords[1])
+            texColor = material.texture.getColor(
+                intersect.texcoords[0], intersect.texcoords[1])
             if texColor is not None:
                 finalColor *= np.array(texColor)
 
@@ -187,8 +200,7 @@ class Raytracer(object):
         g = min(1, finalColor[1])
         b = min(1, finalColor[2])
 
-        return (r,g,b)
-
+        return (r, g, b)
 
     def glRender(self):
         # Proyeccion
@@ -206,12 +218,12 @@ class Raytracer(object):
                 Py *= t
 
                 direction = V3(Px, Py, -self.nearPlane)
-                direction = direction / np.linalg.norm(direction)
+                direction = gm.Normalize(direction)
 
                 rayColor = self.cast_ray(self.camPosition, direction)
 
                 if rayColor is not None:
-                    rayColor = color(rayColor[0],rayColor[1],rayColor[2])
+                    rayColor = color(rayColor[0], rayColor[1], rayColor[2])
                     self.glPoint(x, y, rayColor)
 
     def glFinish(self, filename):
@@ -223,7 +235,7 @@ class Raytracer(object):
             file.write(dword(0))
             file.write(dword(14 + 40))
 
-            #InfoHeader
+            # InfoHeader
             file.write(dword(40))
             file.write(dword(self.width))
             file.write(dword(self.height))
@@ -236,13 +248,7 @@ class Raytracer(object):
             file.write(dword(0))
             file.write(dword(0))
 
-            #Color table
+            # Color table
             for y in range(self.height):
                 for x in range(self.width):
                     file.write(self.pixels[x][y])
-
-
-
-
-
-
